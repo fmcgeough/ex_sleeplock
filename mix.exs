6a138a767bd4d2221c8434d67e081c3ebe89bd42
Frank McGeough <fmcgeough@pagerduty.com>
2024-06-25 11:46:52 -0400

    Allow Elixir 13

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExSleeplock.MixProject do
     [
       app: :ex_sleeplock,
       version: "1.0.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
